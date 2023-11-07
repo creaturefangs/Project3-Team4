e837380be6b8fba8ca2ae7496fed24be8c1e34c7
creaturefangs <126419720+creaturefangs@users.noreply.github.com>
2023-11-07 09:02:08 -0500

    player ui push

--- Team4-Project3/Assets/SCRIPTS/UI/PauseManager.cs
+++ Team4-Project3/Assets/SCRIPTS/UI/PauseManager.cs
@@ -36,7 +36,6 @@ public class PauseManager : MonoBehaviour
     public void Resume()
     {
 
-
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         Time.timeScale = 1f;
