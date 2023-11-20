c0cac3f3e360ab0844b11c95ca30bf66b792c4b1
ccurtis-waketech <kjcurtis@my.waketech.edu>
2023-11-19 19:55:35 -0500

    Active Item and Item Hotkey Fix
    New in-hand object prefabs will only be instantiated if they do not already exist and players can scroll through tools.

--- Team4-Project3/Assets/SCRIPTS/PLAYER/ObjectinHandController.cs
+++ Team4-Project3/Assets/SCRIPTS/PLAYER/ObjectinHandController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,34 +18,45 @@ public class ObjectinHandController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E)) // Press 'E' key to pick up an object
         {
-            // Press 'E' key to pick up an object
             PickUpObject();
         }
 
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q)) // Press 'Q' key to put away the object
         {
-            // Press 'Q' key to put away the object
             PutAwayObject();
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1)) // Press '1' key to pull out the fishing net
         {
-            // Press '1' key to pull out the fishing net
-            PullOutObject(0);
+            //PullOutObject(0);
+            SwitchItem(0);
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+        if (Input.GetKeyDown(KeyCode.Alpha2)) // Press '2' key to pull out the shovel
         {
-            // Press '2' key to pull out the shovel
-            PullOutObject(1);
+            //PullOutObject(1);
+            SwitchItem(1);
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha3))
+        if (Input.GetKeyDown(KeyCode.Alpha3)) // Press '3' key to pull out the fishing pole
         {
-            // Press '3' key to pull out the fishing pole
-            PullOutObject(2);
+            //PullOutObject(2);
+            SwitchItem(2);
+        }
+
+        if (Input.GetAxisRaw("Mouse ScrollWheel") < 0)
+        {
+            int currentItem = ObjectIndex();
+            if (currentItem == 0) { SwitchItem(hand.transform.childCount - 1); }
+            else { SwitchItem(currentItem - 1); }
+        }
+        else if (Input.GetAxisRaw("Mouse ScrollWheel") > 0)
+        {
+            int currentItem = ObjectIndex();
+            if (currentItem == hand.transform.childCount - 1) { SwitchItem(0); }
+            else { SwitchItem(currentItem + 1); }
         }
     }
 
@@ -99,6 +111,41 @@ public class ObjectinHandController : MonoBehaviour
         }
     }
 
+    private void SwitchItem(int index)
+    {
+        if (index >= 0 && index < hand.transform.childCount)
+        {
+            for (var i = 0; i < hand.transform.childCount; i++)
+            {
+                if (i == index) { hand.transform.GetChild(i).gameObject.SetActive(true); }
+                else { hand.transform.GetChild(i).gameObject.SetActive(false); }
+            }
+
+        }
+        else if (tools[index]) // If it's a valid tool but not yet instantiated...
+        {
+            foreach (Transform child in hand.transform) { child.gameObject.SetActive(false); } // Sets all other tools as inactive.
+            GameObject toolPrefab = tools[index];
+            GameObject spawnedTool = Instantiate(toolPrefab, hand.transform);
+            spawnedTool.name = spawnedTool.name.Split("(Clone)")[0]; // Removes the "Clone" ending from the game object when instantiated.
+            spawnedTool.transform.SetSiblingIndex(index); // Orders the child to the correct position among the parent object children.
+            spawnedTool.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Invalid tool index: " + index);
+        }
+    }
+
+    private int ObjectIndex()
+    {
+
+        for (var i = 0; i < hand.transform.childCount; i++)
+        {
+            if (hand.transform.GetChild(i).gameObject.activeSelf) { return i; }
+        }
+        return 0;
+    }
 
     string FindObjectInHand()
     {
